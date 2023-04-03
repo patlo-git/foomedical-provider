605002e42f27353933b7b33861ae3626bd2fce2e
patlo-git <56564279+patlo-git@users.noreply.github.com>
2023-04-03 12:52:00 -0700

    Fix SignInPage tests

--- src/pages/SignInPage.test.tsx
+++ src/pages/SignInPage.test.tsx
@@ -3,24 +3,26 @@ import { MedplumProvider } from '@medplum/react';
 import { act, fireEvent, render, screen } from '@testing-library/react';
 import crypto from 'crypto';
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import { TextEncoder } from 'util';
-import { SignInForm } from '@medplum/react';
+import { SignInPage } from './SignInPage';
 
 const medplum = new MockClient();
 
-describe('SignInPage', () => {
-  function setup(url = '/signin'): void {
+async function setup(url='/signin', medplum = new MockClient()): Promise<void> {
+  await act(async() => {
     render(
-      <BrowserRouter>
+      <MemoryRouter
+        initialEntries={[url]} initialIndex={0}>
         <MedplumProvider medplum={medplum}>
-          <SignInForm>
-          </SignInForm>
+          <SignInPage />
         </MedplumProvider>
-      </BrowserRouter>
+      </MemoryRouter>
     );
-  }
+  })
+}
 
+describe('SignInPage', () => {
   beforeAll(() => {
     Object.defineProperty(global, 'TextEncoder', {
       value: TextEncoder,
@@ -30,26 +32,50 @@ describe('SignInPage', () => {
       value: crypto.webcrypto,
     });
   });
+  
+  beforeEach(async () => {
+    await setup();
+  })
 
-  test('Renders a sign in button', async () => {
-    setup();
-
-    expect(screen.getByRole('button', { name: 'Sign in' })).toBeInTheDocument();
+  test('Renders Sign In Page', async () => {
+    expect(screen.getByText('Medplum Logo')).toBeInTheDocument();
+    
+    expect(screen.getByText('Sign in to Foo Provider')).toBeInTheDocument();
   });
-
-  test('Successfully fires sign in form events', async () => {
-    setup();
-
+  
+  test('Successfully fires sign in page events', async () => {
+    // input email
     await act(async () => {
       fireEvent.change(screen.getByLabelText('Email *'), { target: { value: 'admin@example.com' } });
     });
-
+    
+    // click next
     await act(async () => {
-      fireEvent.change(screen.getByLabelText('Password *'), { target: { value: 'password' } });
+      fireEvent.click(screen.getByRole('button', { name: 'Next' }));
     });
-
+    
+    // input invalid password
+    await act(async () => {
+      fireEvent.change(screen.getByLabelText('Password *'), { target: { value: 'notpassword' } });
+    });
+    
+    // click sign in
     await act(async () => {
       fireEvent.click(screen.getByRole('button', { name: 'Sign in' }));
     });
+    
+    expect(screen.getByText('Invalid password')).toBeInTheDocument();
+    
+    // input valid password
+    await act(async () => {
+      fireEvent.change(screen.getByLabelText('Password *'), { target: { value: 'password' } });
+    });
+    
+    // click sign in
+    await act(async () => {
+      fireEvent.click(screen.getByRole('button', { name: 'Sign in' }));      
+    });
+    
+    expect(screen.getByText('Success')).toBeInTheDocument();
   });
 });
